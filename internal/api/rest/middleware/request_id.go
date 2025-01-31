2055797d99108d0f55b3a666ab848a1fd90a4bf6
1plam <77026826+1plam@users.noreply.github.com>
2025-01-31 13:03:31 +0100

    feat(security): Add HTTP headers middleware

---
+++ internal/api/rest/middleware/request_id.go
@@ -0,0 +1,17 @@
+package middleware
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
+
+func RequestID() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		requestID := c.Get("X-Request-ID")
+		if requestID == "" {
+			requestID = uuid.New().String()
+			c.Set("X-Request-ID", requestID)
+		}
+		return c.Next()
+	}
+}
